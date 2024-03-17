fa7a208c9fd5a2c2f8ae436baa0f1423a79d08e4
Sameer-Shahzada <md.shahzada1606@gmail.com>
2024-03-17 13:52:50 +0530

    card-props-array

---
+++ 5-card-props-array/src/components/Sdata.js
@@ -0,0 +1,33 @@
+const Sdata = [
+    {
+        sname: "Dark",
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "Extra Curricular",
+        imgsrc: "https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "Stranger Things",
+        imgsrc: "https://wallpapercave.com/wp/wp1917154.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "The Vampire Diaries",
+        imgsrc: "https://images.justwatch.com/poster/8589251/s592",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "My First-2 Love",
+        imgsrc: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "A Netflix Original Series",
+        links: "https://www.netflix.com/in/title/81026700?source=35", 
+    }
+]
+export default Sdata;
